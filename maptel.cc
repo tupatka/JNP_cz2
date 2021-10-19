6276a6033392c727a96e109c2e4e153f9a40672f
NanoUser3000 <92107749+NanoUser3000@users.noreply.github.com>
2021-10-19 18:46:04 +0200

    zaczynamy
    Funkcja która nic nie robi, ale kompilator nie krzyczy przy kompilacji pliku .c z niej korzystającego.

---
+++ maptel.cc
@@ -0,0 +1,10 @@
+#include "maptel.h"
+#include <map>
+
+namespace A {
+std::map<int, int> mappa;
+}
+
+unsigned long maptel_create(void) {
+    return 3;
+}
---
+++ maptel.h
@@ -0,0 +1,34 @@
+#ifndef MAPTEL_H
+#define MAPTEL_H
+
+  #define TEL_NUM_MAX_LEN (size_t)22   //! ok?
+  // Tworzy słownik i zwraca liczbę naturalną będącą jego identyfikatorem.
+  #ifdef __cplusplus
+  extern "C" {
+  #endif
+
+    unsigned long maptel_create(void);
+    /*
+      // Usuwa słownik o identyfikatorze id.
+      void maptel_delete(unsigned long id);
+
+      // Wstawia do słownika o identyfikatorze id informację o zmianie numeru
+      // tel_src na numer tel_dst. Nadpisuje ewentualną istniejącą informację.
+      void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst);
+
+      // Jeśli w słowniku o identyfikatorze id jest informacja o zmianie numeru
+      // tel_src, to ją usuwa. W przeciwnym przypadku nic nie robi.
+      void maptel_erase(unsigned long id, char const *tel_src);
+
+      // Sprawdza, czy w słowniku o identyfikatorze id jest zapisana zmiana numeru
+      // tel_src. Podąża ciągiem kolejnych zmian. Zapisuje zmieniony numer w tel_dst.
+      // Jeśli nie ma zmiany numeru lub zmiany tworzą cykl, to zapisuje w tel_dst
+      // numer tel_src. Wartość len to rozmiar przydzielonej pamięci wskazywanej
+      // przez tel_dst.
+      void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len);
+  */
+  #ifdef __cplusplus
+  }
+  #endif
+
+#endif /* MAPTEL_H */
