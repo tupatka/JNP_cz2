43d1b192726a5dd63ddd98bb6f70c6a7c98e8a8d
NanoUser3000 <92107749+NanoUser3000@users.noreply.github.com>
2021-10-20 22:47:21 +0200

    Sensowniejsze implementacje i nowe eksperymenty

--- maptel.cc
+++ maptel.cc
@@ -1,78 +1,76 @@
 #include <iostream>
 #include "maptel.h"
 #include <map>
-#include <set>
-#include <cstring>
 #include <string>
+#include <cassert>
+
 using book_t = std::map<std::string, std::string>;//<tel_src, tel_dst>
 using library_t = std::map<long, book_t>;//<id słownika, słownik>
-//id_t = unsigned long //!???
+//using id_t = unsigned long //!?
 
 static library_t library;
 
+/*
+pytania
+	-czy usunięcie nieistniejącego słownika to błąd?
+	-co ze stwarzaniem istniejącego?
+
+	-czy wstawienie (maptel_insert) do nie istniejącego słownika to błąd? jeśli tak,
+	to set kluczy (id) lub przeglądanie kluczy (wsm to drugie, find() na pewno idzie binsearchem, podobno count() też)
+	//w unordered map można w czasie stały???
+
+	library[id][tel_src] = tel_dst; <-- działa nawet jeśli słownik library[id] nie został stworzony wcześniej
+	-zatem może set istniejących id, np. jeśli insert(nie_istniejący, tel_src, tel_dst) jest błędem, trzeba sprawdzać czy istnieje
+	-w maptel_create() pewnie i tak najlepiej tworzyć słowniki o kolejnych id (tj. 0, 1, 2,...) 
+*/
+
 unsigned long maptel_create(void) {
-    book_t b;
-    library[3] = b;
-    return 3;
+    static unsigned long id = 0;
+    assert(id != -1);
+    library[id];//teraz para (id, pusty_slownik) jest w mapie (library)
+    ++id;
+    return id - 1;
 }
 
 void maptel_delete(unsigned long id) {
+	if (library.find(id) == library.end()) {//!Err
+		std::cerr << "deleting non existing book no " << id << std::endl;
+	}
 	library.erase(id);
 }
 
-void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
-	if(id == -1) {
-		std::cout << "diagnostyka" << std::endl;
-		for(auto tel : library[3]) {
-			std::cout << tel.first << " " << tel.second << std::endl;
+/**
+ * Wypisuje zawartość wszystkich słowników
+ */
+void diag() {
+	std::cerr << "diagnostyka" << std::endl;
+	for (auto book : library) {
+		std::cerr << "book no " << book.first << std::endl;
+		for (auto tel : book.second) {//tel można nazwać change
+			std::cerr << tel.first << " -> " << tel.second << std::endl;
 		}
+	}
+	std::cerr << std::endl;
+}
+
+/**
+ * Użyta z argumentem id równym -1 wywołuje diag() i nie insertuje,
+ * a argumenty tel_sth nie mają znaczenia
+ */
+void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
+	//! //Diagnostyczne
+	if(id == -1) {
+		diag();
 		return;
 	}
-	//!spr wartości
-	if (!library.count(id)) {
-		book_t b;
-		library[id] = b;
+
+	//!Uwaga: dodać sprawdzenie danych
+	
+	std::cerr << "inserting into " << id << ": " <<tel_src << " -> " << tel_dst << std::endl;
+	if (library.find(id) == library.end()) {//!Err
+		std::cerr << "inserting into non existing book " << id << std::endl;
 	}
-	std::cout << "inserting: " << tel_src << " " << tel_dst << std::endl;
-	library[id][tel_src] = tel_dst;
-}
+	library[id][tel_src] = tel_dst;	//działa nawet jeśli słownik library[id] nie istnieje
+									//po prostu go tworzy i umieszcza tam tel_src jako klucz
 
-void maptel_erase(unsigned long id, char const *tel_src) {
-    book_t book = library[id];
-
-    auto tel_it = book.find(tel_src);
-    if (tel_it != book.end()) {
-        book.erase(tel_it);
-    }
-}
-
-void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
-    book_t book = library[id];
-
-    auto tel_it = book.find(tel_src);
-    if (tel_it == book.end()) { // Nie było zmiany numeru.
-        // mam nadzieje ze da sie to zrobic ladniej
-        tel_dst = (char*)malloc(strlen(tel_src + 1));
-        strcpy(tel_dst, tel_src);
-        return;
-    }
-
-    std::set<char*> set; // Do szukania powtórzeń numerów.
-    auto tel_dst_it = tel_it;
-    while (tel_it != book.end()) {
-
-        // TO NIE DZIALA I NIE WIEM JAK ZMIENIC
-        char* tel_curr = tel_it->first; // chce zdjac napis z seta, ale moze w sumie by wystarczyl tylko wskaznik a nie gleboka kopia
-        if (set.find(tel_curr) != set.end()) { // Mamy cykl.
-            tel_dst = (char*)malloc(strlen(tel_src + 1));
-            strcpy(tel_dst, tel_src);
-            return;
-        }
-
-        // TO TAK SAMO
-        char* tel_next = tel_it->second;
-        set.insert(tel_next);
-        tel_dst_it = tel_it;
-        tel_it = book.find(tel_next);
-    }
 }
--- maptel_test1.c
+++ maptel_test1.c
@@ -17,18 +17,19 @@ int main() {
   maptel_insert(id, t416, t401);
   maptel_insert(id, t401, t333);
 
-//!
+//! tego nie ma w oficjalnym teście fragmentu
     char* sometel = malloc(5* sizeof(char));
     for (int i = 0; i < 5; ++i) {
       sometel[i] = '0' + i;
     }
-    maptel_insert(id, sometel, sometel);
+    maptel_insert(4, sometel, sometel);
     maptel_insert(-1, sometel, sometel);
 
     sometel[2] = 'e';
     maptel_insert(-1, sometel, sometel);
     sometel = NULL;
     maptel_insert(-1, sometel, sometel);
+    maptel_delete(5);
 //!
 
 /*  maptel_transform(id, t112, tel, TEL_NUM_MAX_LEN + 1);
