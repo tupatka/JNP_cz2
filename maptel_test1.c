3dc21324e35cb3afe7e06854e798ca59a5b2e4e4
NanoUser3000 <92107749+NanoUser3000@users.noreply.github.com>
2021-10-20 23:32:31 +0200

    Naprawiony problematyczny kod w transform, pozostało je dokończyć

--- maptel_test1.c
+++ maptel_test1.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <string.h>
 #include "maptel.h"
-#include <stdlib.h> //!
+
 static const char t112[] = "112";
 static const char t997[] = "997";
 static const char t416[] = "225544416";
@@ -13,28 +13,10 @@ int main() {
   char tel[TEL_NUM_MAX_LEN + 1]; /* +1 na terminalne zero */
 
   id = maptel_create();
-  maptel_insert(id, t112, t997);
+/*  maptel_insert(id, t112, t997);
   maptel_insert(id, t416, t401);
   maptel_insert(id, t401, t333);
-
-//! tego nie ma w oficjalnym teście fragmentu
-    char* sometel = malloc(5* sizeof(char));
-    for (int i = 0; i < 5; ++i) {
-      sometel[i] = '0' + i;
-    }
-    maptel_insert(4, sometel, sometel);
-    maptel_insert(-1, sometel, sometel);
-
-    sometel[2] = 'e';
-    maptel_insert(-1, sometel, sometel);
-    sometel = NULL;
-    maptel_insert(-1, sometel, sometel);
-    maptel_delete(5);
-    maptel_erase(3, "abba");
-    maptel_insert(-1, "", "");
-//!
-
-/*  maptel_transform(id, t112, tel, TEL_NUM_MAX_LEN + 1);
+  maptel_transform(id, t112, tel, TEL_NUM_MAX_LEN + 1);
   assert(strcmp(tel, t997) == 0);
   maptel_transform(id, t333, tel, TEL_NUM_MAX_LEN + 1);
   assert(strcmp(tel, t333) == 0);
